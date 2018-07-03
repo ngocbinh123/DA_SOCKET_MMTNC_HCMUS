cb22e6e1e5a1fd74271ece2cce197b9c30d6c579
Nguyen Ngoc Binh <nnbinh0301@gmail.com>
2018-07-03 16:56:58 +0700

    remove redundant code

--- src/flexibleBase/src/main/java/hcmus/Constant.java
+++ src/flexibleBase/src/main/java/hcmus/Constant.java
@@ -4,9 +4,4 @@ public class Constant {
     public static final String MSG_WHO_ARE_YOU = "Who are you?";
     public static final String MSG_PLS_SEND_YOUR_FILES = "Please send me your files";
     public static final String MSG_QUIT = "quit";
-
-    public enum TYPE {
-        CLIENT(),
-        NODE()
-    }
 }
